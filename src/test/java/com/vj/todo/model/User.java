f7ed429eb399256e10bad9650e35622ec443969c
Vijendra Aithal <vijendra.aithal@gmail.com>
2025-06-09 20:09:39 +0530

    Implemented User model, moved faker code into User model's constructor, getters and setters

---
+++ src/test/java/com/vj/todo/model/User.java
@@ -0,0 +1,50 @@
+package com.vj.todo.model;
+
+import com.github.javafaker.Faker;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public User() {
+        Faker faker = new Faker();
+        firstName = faker.name().firstName();
+        lastName = faker.name().lastName();
+        email = firstName + "." + lastName + faker.number().numberBetween(100, 500) + "@email.com";
+        password = "Test123!";
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- src/test/java/com/vj/todo/testcases/UserTest.java
+++ src/test/java/com/vj/todo/testcases/UserTest.java
@@ -2,6 +2,7 @@ package com.vj.todo.testcases;
 
 import com.github.javafaker.Faker;
 import com.vj.todo.base.BaseTest;
+import com.vj.todo.model.User;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,17 +15,13 @@ public class UserTest extends BaseTest {
 
     @Test
     public void shouldBeAbleToRegisterToApp() {
-        Faker faker = new Faker();
-        String firstName = faker.name().firstName();
-        String lastName = faker.name().lastName();
-        String email = firstName + "." + lastName + faker.number().numberBetween(100, 500) + "@email.com";
-
+        User user = new User();
         driver.get("https://todo.qacart.com/signup");
-        driver.findElement(By.cssSelector("[data-testid='first-name']")).sendKeys(firstName);
-        driver.findElement(By.cssSelector("[data-testid='last-name']")).sendKeys(lastName);
-        driver.findElement(By.cssSelector("[data-testid='email']")).sendKeys(email);
-        driver.findElement(By.cssSelector("[data-testid='password']")).sendKeys("Test123!");
-        driver.findElement(By.cssSelector("[data-testid='confirm-password']")).sendKeys("Test123!");
+        driver.findElement(By.cssSelector("[data-testid='first-name']")).sendKeys(user.getFirstName());
+        driver.findElement(By.cssSelector("[data-testid='last-name']")).sendKeys(user.getLastName());
+        driver.findElement(By.cssSelector("[data-testid='email']")).sendKeys(user.getEmail());
+        driver.findElement(By.cssSelector("[data-testid='password']")).sendKeys(user.getPassword());
+        driver.findElement(By.cssSelector("[data-testid='confirm-password']")).sendKeys(user.getPassword());
         driver.findElement(By.cssSelector("[data-testid='submit']")).click();
         boolean isWelcomeMsgDisplayed = driver.findElement(By.cssSelector("[data-testid='welcome']")).isDisplayed();
         Assert.assertTrue(isWelcomeMsgDisplayed);
