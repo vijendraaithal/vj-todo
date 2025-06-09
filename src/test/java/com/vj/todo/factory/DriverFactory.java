f761e6e28474df16abfda9e0c0c612bbee96c8b3
Vijendra Aithal <vijendra.aithal@gmail.com>
2025-06-09 23:54:48 +0530

    Implemented multi browser support

--- src/test/java/com/vj/todo/factory/DriverFactory.java
+++ src/test/java/com/vj/todo/factory/DriverFactory.java
@@ -2,13 +2,24 @@ package com.vj.todo.factory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 import java.time.Duration;
 
 public class DriverFactory {
 
     public WebDriver initializeDriver() {
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver;
+        String browser = System.getProperty("browser", "CHROME");
+        switch (browser) {
+            case "CHROME" -> driver = new ChromeDriver();
+            case "FF" ->  driver = new FirefoxDriver();
+            case "SAFARI" ->  driver = new SafariDriver();
+            case "EDGE" -> driver = new EdgeDriver();
+            default -> throw new RuntimeException("Browser is not supported");
+        }
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.manage().window().maximize();
         return driver;
