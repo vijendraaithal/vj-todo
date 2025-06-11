7569811411d94f5186204fab0641c2ebb3ee1a5d
Vijendra Aithal <vijendra.aithal@gmail.com>
2025-06-11 17:58:02 +0530

    Update

--- src/test/java/com/vj/todo/factory/DriverFactory.java
+++ src/test/java/com/vj/todo/factory/DriverFactory.java
@@ -2,8 +2,10 @@ package com.vj.todo.factory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.safari.SafariDriver;
 
 import java.time.Duration;
@@ -14,8 +16,16 @@ public class DriverFactory {
         WebDriver driver;
         String browser = System.getProperty("browser", "CHROME");
         switch (browser.toUpperCase()) {
-            case "CHROME" -> driver = new ChromeDriver();
-            case "FF" ->  driver = new FirefoxDriver();
+            case "CHROME" -> {
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--headless");
+                driver = new ChromeDriver();
+            }
+            case "FF" ->  {
+                FirefoxOptions options = new FirefoxOptions();
+                options.addArguments("--headless");
+                driver = new FirefoxDriver();
+            }
             case "SAFARI" ->  driver = new SafariDriver();
             case "EDGE" -> driver = new EdgeDriver();
             default -> throw new RuntimeException("Browser is not supported");
